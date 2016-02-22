7eaddffabcacc900b20b4b9bcbe1879b2fc95896
delta-analytics <frank.jaeger@delta-analytics.de>
2016-02-22 22:45:07 +0100

    HitranParameters == Class for all molecules

--- src/main/java/deltaanalytics/gui/math/HitranParameters.java
+++ src/main/java/deltaanalytics/gui/math/HitranParameters.java
@@ -1,39 +1,39 @@
 package deltaanalytics.gui.math;
 
+import java.util.Arrays;
+
 /** ToDo get parameters from GUI
  *  vector of initial parameters for use of Hitran data base
 */
 public class HitranParameters {
-    private boolean[] callHitran;  // which moleclue to investigate
+    private boolean[] callHitran;  // which moleclue to investigate 1=H2O  2=CO2  3=N2O  4=CO  5=CH4  6=NO  7=NO2
     private double[] lowWN;  // low wavenumbers
     private double[] highWN;  // high wavenumbers
-    private int molecule;  // molecule 1=H2O  2=CO2  3=N2O  4=CO  5=CH4  6=NO  7=NO2
-    private int baselineStep;  // range of wavenumbers
-    private double stp;;  // frequency spacing in cm-1, corresponds to 0.04 resolution in cm-1
-    private double intensThres1; // intensity threshold1 
+    private int[] baselineStep;  // sampling point interval to fit a baseline curve
+    private double[] stp;  // default 0.02; frequency hitran spacing in cm-1, 0.02 corresponds to 0.04 resolution in cm-1
+    private double intensThres1; // intensity threshold1 1e-25
     private int isotopo1;  //default 1
-    private double intensThres2; // intensity threshold2 
+    private double intensThres2; // intensity threshold2 1e-25
     private int isotopo2;  // default 2
-    private double sf;  // scaling factor isotopo2
-    private double Temp;  // Temperature 40 °C
-    private double Patm;  // total Pressure in ATM units
-    private int Dd;  // wings of each line in wavenumbers of cm-1
+    private double[] sf;  // default 1; scaling factor isotopo2
+    private double Temp;  // default = 313 Kelvin; Temperature 40 °C
+    private double Patm;  // default = 1 atm; total Pressure in ATM units
+    private int[] Dd;  // default = 5; wings of each line in wavenumbers of cm-1
     
     public HitranParameters(){
-        this.callHitran = new boolean[] {true, true, true, true, false, false, true};
+        this.callHitran = new boolean[] {false, false, false, false, false, false, false};
         this.lowWN = new double[] {3860, 3470, 2500, 2080, 2900, 3730, 2840};
         this.highWN = new double[] {3965, 3760, 2600, 2141, 3165, 3780, 2940};
-        this.molecule = 4;
-        this.baselineStep = 0;
-        this.stp = 0.02;
+        this.baselineStep = new int[] {0, 0, 50, 0, 0, 50, 50};
+        this.stp = new double[] {0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02};
         this.intensThres1 = 1e-25;
         this.isotopo1 = 1;
         this.intensThres2 = 1e-25;
         this.isotopo2 = 2;
-        this.sf = 1;
+        this.sf = new double[] {1, 1, 1, 1, 1, 1, 1};
         this.Temp = 313;
         this.Patm = 1;
-        this.Dd = 5;
+        this.Dd = new int[] {5, 5, 5, 5, 5, 5, 5};
     }
 
     public boolean[] getCallHitran() {
@@ -60,27 +60,19 @@ public class HitranParameters {
         this.highWN = highWN;
     }
 
-    public int getMolecule() {
-        return molecule;
-    }
-
-    public void setMolecule(int molecule) {
-        this.molecule = molecule;
-    }
-
-    public int getBaselineStep() {
+    public int[] getBaselineStep() {
         return baselineStep;
     }
 
-    public void setBaselineStep(int baselineStep) {
+    public void setBaselineStep(int[] baselineStep) {
         this.baselineStep = baselineStep;
     }
 
-    public double getStp() {
+    public double[] getStp() {
         return stp;
     }
 
-    public void setStp(double stp) {
+    public void setStp(double[] stp) {
         this.stp = stp;
     }
 
@@ -116,11 +108,11 @@ public class HitranParameters {
         this.isotopo2 = isotopo2;
     }
 
-    public double getSf() {
+    public double[] getSf() {
         return sf;
     }
 
-    public void setSf(double sf) {
+    public void setSf(double[] sf) {
         this.sf = sf;
     }
 
@@ -140,14 +132,26 @@ public class HitranParameters {
         this.Patm = Patm;
     }
 
-    public int getDd() {
+    public int[] getDd() {
         return Dd;
     }
 
-    public void setDd(int Dd) {
+    public void setDd(int[] Dd) {
         this.Dd = Dd;
     }
 
-    
+    @Override
+    public String toString() {
+        return "HitranParameters{" + "callHitran=" + Arrays.toString(callHitran)
+                + ", lowWN=" + Arrays.toString(lowWN) + ", highWN=" + Arrays.toString(highWN)
+                + ", baselineStep=" + Arrays.toString(baselineStep)
+                + ", stp=" + Arrays.toString(stp)
+                + ", intensThres1=" + intensThres1 + ", isotopo1=" + isotopo1
+                + ", intensThres2=" + intensThres2 + ", isotopo2=" + isotopo2
+                + ", sf=" + Arrays.toString(sf)
+                + ", Temp=" + Temp + ", Patm=" + Patm
+                + ", Dd=" + Arrays.toString(Dd) + '}';
+    }
+  
     
 }
